b70f26504181c849ad6f10133d83b44d75ceabc5
Yarik0804 <60669032+Yarik0804@users.noreply.github.com>
2020-02-21 21:33:15 +0200

    lesson11

---
+++ html51/lesson11/scripts/main.js
@@ -0,0 +1,9 @@
+function startLogic() {
+    var newHtml = '<em>New Header</em>';
+    document.getElementById('headeritem').innerHTML = newHtml;
+}
+startLogic();
+
+function clickHandler() {
+    alert('You clicked me!');
+}
